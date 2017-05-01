873ce4a1063bc5baf7f31e9997d60cfa4adf5b6e
hubodashi <hubodashi@aliyun.com>
2017-05-02 02:01:28 +0800

    add something of step 1

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,5 @@
 class JobsController < ApplicationController
+  before_action :validate_search_key, only: [:search]
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   def show
     @job=Job.find(params[:id])
@@ -16,6 +17,22 @@ class JobsController < ApplicationController
     else
       Job.published.recent
     end
+
+    if params[:c].present?
+        @city = params[:c]
+        @jobs = @jobs.where(:city => @city)
+      end
+
+    if params[:d].present?
+        @category = params[:d]
+        @jobs = @jobs.where(:category => @category)
+      end
+
+      if params[:e].present?
+        @company = params[:e]
+        @jobs = @jobs.where(:company => @company)
+      end
+
   end
   def new
     @job=Job.new
@@ -44,8 +61,27 @@ class JobsController < ApplicationController
     @job.destroy
     redirect_to jobs_path
   end
+  def search
+  if @query_string.present?
+    search_result = Job.published.ransack(@search_criteria).result(:distinct => true)
+    @jobs = search_result.recent.paginate(:page => params[:page], :per_page => 5 )
+  end
+end
  private
  def job_params
-   params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
+   params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :city, :company, :category, :is_hidden)
  end
+ protected
+  def validate_search_key
+    @query_string = params[:q].gsub(/\\|\'|\/|\?/, "")
+    if params[:q].present?
+      @search_criteria =  {
+        title_or_company_or_city_cont: @query_string
+      }
+    end
+  end
+
+  def search_criteria(query_string)
+    { :title_cont => query_string }
+  end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -12,6 +12,9 @@ class Job < ApplicationRecord
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0 }
+  validates :city, presence: true
+  validates :category, presence: true
+  validates :city, presence: true
   scope :published, -> { where(is_hidden: false) }
   scope :recent, -> { order('created_at DESC') }
 end
